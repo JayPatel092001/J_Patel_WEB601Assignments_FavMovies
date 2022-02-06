9c9f40c13e0c2152ee1e2d37d1d4e062fca5ace1
Jay Patel <w0778384@myscc.ca>
2022-02-06 15:37:14 -0800

    appcomponent.html

--- J_Patel_FavMovies/src/app/app.component.ts
+++ J_Patel_FavMovies/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { Movie } from './helper-files/movie-interface';
+
 import { MovieList } from './helper-files/movie-list';
 
 @Component({
@@ -10,21 +10,6 @@ import { MovieList } from './helper-files/movie-list';
 export class AppComponent {
   title = 'J_Patel_FavMovies';
 
-  constructor() {
-    
-   
-
-    let names: any;
-   
-    names = {
-    id:1,
-    title: "dj",
-    description: "gsahdfgasd",
-    creator:"mohan",
-    imgURL: "jhedghdsgf",
-
-    };
-       
 
   }
-}
+
